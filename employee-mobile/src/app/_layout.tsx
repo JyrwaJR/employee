41935cc84f496c4b9d7f1f729f67fd8af9ac2ea2
JyrwaJR <jyrwaboys@gmail.com>
2026-01-30 22:27:36 +0530

    add ssl pinning

--- employee-mobile/src/app/_layout.tsx
+++ employee-mobile/src/app/_layout.tsx
@@ -3,17 +3,10 @@ import { ProviderWrapper } from '../components/provider';
 import './global.css';
 import { initializeSslPinning } from 'react-native-ssl-public-key-pinning';
 import { Stack } from 'expo-router';
+import { SSL_CONFIG } from '../libs/config/networkConfig';
 
 export default function Layout() {
-  initializeSslPinning({
-    'employee-nic.vercel.app': {
-      includeSubdomains: true,
-      publicKeyHashes: [
-        'hS5jJ4P+iS6icS6iS6icS6iS6icS6iS6icS6iS6icS6=', // Leaf hash
-        'C5+lpZ7tc+LRfQ1S3s1S3s1S3s1S3s1S3s1S3s1S3s1=', // Let's Encrypt R10 (Backup)
-      ],
-    },
-  });
+  initializeSslPinning(SSL_CONFIG);
 
   return (
     <ProviderWrapper>
---
+++ employee-mobile/src/libs/config/networkConfig.ts
@@ -0,0 +1,17 @@
+// src/config/networkConfig.ts
+
+// 1. Define the pins (Safety Net + Env)
+const PINS = [
+  process.env.EXPO_PUBLIC_CURRENT_LEAF_HASH,
+  process.env.EXPO_PUBLIC_NEXT_LEAF_HASH,
+  'C5+lpZ7tc+LRfQ1S3s1S3s1S3s1S3s1S3s1S3s1S3s1=', // R10 Backup
+  'Di5jJ4P+iS6icS6iS6icS6iS6icS6iS6icS6iS6icS6=', // E5 Backup
+].filter(Boolean) as string[];
+
+// 2. Format it correctly for the library
+export const SSL_CONFIG = {
+  'employee-nic.vercel.app': {
+    includeSubdomains: true,
+    publicKeyHashes: PINS,
+  },
+};
