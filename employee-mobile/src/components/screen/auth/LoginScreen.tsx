77bc8367f7801c28c7ae13a8067178b4328afec7
JyrwaJR <jyrwaboys@gmail.com>
2026-01-31 23:14:34 +0530

    remove not needed

--- employee-mobile/src/components/screen/auth/LoginScreen.tsx
+++ employee-mobile/src/components/screen/auth/LoginScreen.tsx
@@ -8,7 +8,7 @@ import {
   KeyboardAvoidingView,
   Platform,
 } from 'react-native';
-import { Link, useRouter } from 'expo-router';
+import { Link } from 'expo-router';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
