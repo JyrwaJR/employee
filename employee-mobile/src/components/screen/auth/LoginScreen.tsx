f1a45a6180479f351bb1aefdeb19cfec1be1b9ef
JyrwaJR <jyrwaboys@gmail.com>
2026-02-02 22:18:45 +0530

    change ui color

--- employee-mobile/src/components/screen/auth/LoginScreen.tsx
+++ employee-mobile/src/components/screen/auth/LoginScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   View,
-  Text,
   TouchableOpacity,
   Alert,
   SafeAreaView,
@@ -21,6 +20,9 @@ import { toast } from 'sonner-native';
 import { TokenStoreManager } from '@/src/libs/stores/auth';
 import { useAuth } from '@/src/hooks/auth/useAuth';
 import { http } from '@/src/utils/http';
+import { useThemeStore } from '@/src/store/theme';
+import { Text } from '@/src/components/ui/text';
+import { Container } from '../../common/Container';
 
 type LoginFormInputs = z.infer<typeof LoginSchema>;
 
@@ -35,6 +37,7 @@ type LoginResT = {
 
 export const LoginScreen = () => {
   const { refresh } = useAuth();
+  const { theme, toggleTheme } = useThemeStore();
 
   const loginMutation = useMutation({
     mutationFn: (data: LoginFormInputs) => http.post<LoginResT>(AUTH_ENDPOINTS.POST_SIGN_IN, data),
@@ -61,18 +64,19 @@ export const LoginScreen = () => {
   const onSubmit = (data: LoginFormInputs) => loginMutation.mutate(data);
 
   return (
-    <SafeAreaView className="flex-1 bg-white">
+    <Container>
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         className="flex-1 justify-center px-6">
         {/* Header Section */}
         <View className="mb-10 items-center">
-          <View className="mb-6 h-16 w-16 items-center justify-center rounded-2xl bg-gray-100">
+          <View className="mb-6 h-16 w-16 items-center justify-center rounded-2xl bg-blue-600 dark:bg-blue-600">
             {/* Logo Placeholder */}
-            <Text className="text-2xl">✨</Text>
+            <Text variant={'heading'} className='text-white' size={'2xl'}>✨
+            </Text>
           </View>
-          <Text className="text-3xl font-bold tracking-tight text-gray-900">Welcome back</Text>
-          <Text className="mt-2 text-base text-gray-500">
+          <Text variant={'heading'} size={'2xl'} weight={'semibold'}>Welcome back</Text>
+          <Text variant={'subtext'}>
             Please enter your details to sign in.
           </Text>
         </View>
@@ -94,7 +98,7 @@ export const LoginScreen = () => {
           />
 
           <TouchableOpacity className="mb-8 items-end">
-            <Text className="text-sm font-semibold text-blue-600">Forgot password?</Text>
+            <Text variant={'link'}>Forgot password?</Text>
           </TouchableOpacity>
 
           <ModernButton
@@ -103,9 +107,9 @@ export const LoginScreen = () => {
             isLoading={loginMutation.isPending}
           />
 
-          <View className="my-8 flex-row items-center">
+          <View className="my-8 flex-row gap-x-2 items-center">
             <View className="h-[1px] flex-1 bg-gray-200" />
-            <Text className="mx-4 text-xs font-medium uppercase text-gray-400">
+            <Text variant={'subtext'}>
               Or continue with
             </Text>
             <View className="h-[1px] flex-1 bg-gray-200" />
@@ -117,15 +121,13 @@ export const LoginScreen = () => {
             onPress={() => Alert.alert('Google Auth')}
           />
         </View>
-
-        {/* Footer */}
         <View className="mt-10 flex-row justify-center">
-          <Text className="text-gray-500">Don&apos;t have an account? </Text>
+          <Text variant={'subtext'}>Don&apos;t have an account? </Text>
           <Link href={'/auth/sign-up'}>
-            <Text className="font-semibold text-gray-900">Sign up</Text>
+            <Text variant={'link'}>Sign up</Text>
           </Link>
         </View>
       </KeyboardAvoidingView>
-    </SafeAreaView>
+    </Container>
   );
 };
