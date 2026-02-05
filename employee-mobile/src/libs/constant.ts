55cf18b999aac31679c636ce21f19fa0649416bd
JyrwaJR <jyrwaboys@gmail.com>
2026-02-05 19:15:41 +0530

    added is expogo to avoid repeating check when its running in expo go

--- employee-mobile/src/libs/constant.ts
+++ employee-mobile/src/libs/constant.ts
@@ -1,2 +1,5 @@
+import Constants from 'expo-constants';
+
 export const JWT_TOKEN_KEY = 'token';
 export const REFRESH_TOKEN_KEY = 'refresh_token';
+export const isExpoGo = Constants.appOwnership === 'expo';
