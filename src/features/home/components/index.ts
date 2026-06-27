bfb189b2cb79691ae59d6e52cdace13994f9d6f1
JyrwaJR <jyrwaboys@gmail.com>
2026-06-27 23:01:47 +0530

    refactor: add HomeHeader and HomeLeaveEmptyCard to barrel exports

--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -1,4 +1,6 @@
 /** Home dashboard UI components. */
 export * from './active-leave-card';
+export * from './home-header';
+export * from './home-leave-empty-card';
 export * from './leave-preview';
 export * from './quick-actions';
