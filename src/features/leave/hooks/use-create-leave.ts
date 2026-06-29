7292ce25683355369ce5d98e2d95f7d664442791
JyrwaJR <jyrwaboys@gmail.com>
2026-06-29 13:29:55 +0530

    fix: sent employee code when creating leave

--- src/features/leave/hooks/use-create-leave.ts
+++ src/features/leave/hooks/use-create-leave.ts
@@ -2,18 +2,27 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { rpc } from '@utils/api';
 import { METHODS, QUERY_KEYS } from '@utils/constants';
 import { CreateLeaveInputs } from '../validators';
+import { useAuthStore } from '@stores/auth.store';
 
 export function useCreateLeave() {
   const queryClient = useQueryClient();
+  const { emp_cd } = useAuthStore();
   return useMutation({
     mutationKey: QUERY_KEYS.LEAVE.LIST(),
     mutationFn: (data: CreateLeaveInputs) =>
-      rpc<{ id: string }>(METHODS.CREATE_LEAVE_REQUEST, data),
-    onSuccess: (data) => {
+      rpc<{ id: string }>(METHODS.CREATE_LEAVE_REQUEST, {
+        ...data,
+        emp_cd,
+      }),
+    onSuccess: (data, _v, _, context) => {
       if (data.success) {
-        queryClient.invalidateQueries({ queryKey: QUERY_KEYS.LEAVE.LIST() });
+        queryClient.invalidateQueries({ queryKey: context.mutationKey });
         return data;
       }
+      return data;
+    },
+    onError: (error) => {
+      return error;
     },
   });
 }
