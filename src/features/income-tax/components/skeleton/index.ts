798b78c85d75bf3d9cdc957555c0924239f8dc73
JyrwaJR <jyrwaboys@gmail.com>
2026-07-08 23:30:01 +0530

    feat(tax): add skeleton barrel export

---
+++ src/features/income-tax/components/skeleton/index.ts
@@ -0,0 +1,4 @@
+export { TaxListSkeleton } from './tax-list-skeleton';
+export { TaxDetailSkeleton } from './tax-detail-skeleton';
+export { EditTaxSkeleton } from './edit-tax-skeleton';
+export { CreateTaxSkeleton } from './create-tax-skeleton';
