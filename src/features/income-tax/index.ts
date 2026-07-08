502e305f754139e0949ebcc5e0912aa335292a0b
JyrwaJR <jyrwaboys@gmail.com>
2026-07-08 23:23:41 +0530

    feat(tax): add feature barrel export

---
+++ src/features/income-tax/index.ts
@@ -0,0 +1,6 @@
+export * from './utils/constants';
+export * from './components';
+export * from './hooks';
+export * from './screens';
+export * from './types';
+export * from './validators/tax.validator';
